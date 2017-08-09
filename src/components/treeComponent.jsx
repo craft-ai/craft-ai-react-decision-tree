dd7881473c4086062c06e805b904c6c537b53529
Perrine Brunel <perrine.brunel@gmail.com>
2017-08-09 14:44:52 +0200

    use react dimensions to take care of the size of the parent container

--- src/components/treeComponent.jsx
+++ src/components/treeComponent.jsx
@@ -1,5 +1,5 @@
 import _ from 'lodash';
-import Dimensions from 'react-dimensions';
+import ContainerDimensions from 'react-container-dimensions';
 import PropTypes from 'prop-types';
 import React from 'react';
 import Tree from './tree';
@@ -7,19 +7,25 @@ import Tree from './tree';
 class TreeComponent extends React.Component {
   render() {
     return (
-      <Tree
-        height={ this.props.containerHeight }
-        width={ this.props.containerWidth }
-        configuration={ this.props.data.configuration }
-        treeData={ this.props.data.trees[_.keys(this.props.data.trees)[0]] } />
+      <div style={{ width: '100%', height: '100%' }}>
+        <ContainerDimensions>
+          {
+            ({ height, width }) => (
+              <Tree
+                height={ height || 400 }
+                width={ width }
+                configuration={ this.props.data.configuration }
+                treeData={ this.props.data.trees[_.keys(this.props.data.trees)[0]] } />
+            )
+          }
+        </ContainerDimensions>
+      </div>
     );
   }
 }
 
 TreeComponent.propTypes = {
-  data: PropTypes.object.isRequired,
-  containerWidth: PropTypes.number.isRequired,
-  containerHeight: PropTypes.number.isRequired
+  data: PropTypes.object.isRequired
 };
 
-export default Dimensions()(TreeComponent);
+export default TreeComponent;
--- stories/index.js
+++ stories/index.js
@@ -51,18 +51,6 @@ storiesOf('Tree displayed with fixed width', module)
       <Tree data={ tree } />
     </div>
   ))
-  .add('flexbox column', () => (
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        flexGrow:'1',
-        width: 800,
-        border: 'solid 1px black'
-      }}>
-      <Tree data={ tree } />
-    </div>
-  ))
   .add('height 100%', () => (
     <div
       style={{
@@ -105,5 +93,26 @@ storiesOf('Small tree', module)
         border: 'solid 1px black'
       }}>
       <Tree data={ smallTree } />
+    </div>
+  ));
+
+storiesOf('Tree in content', module)
+  .add('bewteen centered text', () => (
+    <div style={{
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'stretch',
+      justifyContent: 'flex-start',
+      flex: '0 1 auto',
+      paddingLeft: 15,
+      paddingRight: 15,
+      backgroundColor: 'cadetblue',
+      margin: '0px 100px'
+    }}>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget lectus lobortis, tempus lectus id, imperdiet erat. Nunc placerat cursus est sit amet ultrices. Donec turpis felis, tincidunt a neque sit amet, bibendum bibendum nulla. Phasellus et lectus sit amet tellus mattis interdum. Pellentesque fermentum sodales pulvinar. Cras at diam tristique, pulvinar quam vel, rutrum leo. Cras sapien libero, euismod nec lectus non, pellentesque congue lorem. Maecenas lacus ligula, accumsan vel erat et, consequat dapibus dui. Nullam massa sapien, venenatis ut massa non, efficitur pulvinar elit. Maecenas dapibus eros non odio bibendum, vel porta nibh iaculis. Integer blandit lorem id blandit placerat. Sed pharetra tristique mauris ut euismod. Vestibulum blandit pellentesque massa nec fermentum. Integer velit eros, malesuada ut tortor id, efficitur scelerisque nibh.</p>
+      <div style={{ height: 500 }}>
+        <Tree data={ tree } />
+      </div>
+      <p>Phasellus gravida urna mi, luctus sagittis dolor scelerisque eu. Pellentesque aliquam justo non ultricies pretium. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed at porttitor urna. Curabitur tincidunt felis vel eros auctor, sed ullamcorper justo rutrum. Sed in luctus neque, at pharetra sapien. Curabitur vitae erat mauris. Maecenas consectetur maximus egestas. Morbi euismod, turpis sit amet eleifend dictum, orci elit consectetur ante, ac ornare orci dui sed lectus. In purus ligula, cursus sit amet posuere quis, maximus eget quam. Nunc ornare metus augue, congue viverra ante gravida ac. Curabitur ultrices suscipit congue. Donec nec leo metus. Nulla ut ipsum malesuada, elementum sapien ac, auctor erat. Cras id sem lorem. Nullam leo leo, auctor in fringilla consectetur, pretium gravida ante.</p>
     </div>
   ));
