3fd8a354c306e0f802c0de545e09e8d35614a8ab
Perrine Brunel <perrine.brunel@gmail.com>
2019-03-13 10:49:13 +0100

    exporting node informations component

--- packages/react-craft-ai-decision-tree/src/index.js
+++ packages/react-craft-ai-decision-tree/src/index.js
@@ -1,4 +1,18 @@
+import DecisionRules from './components/nodeInformation/decisionRules';
 import DecisionTree from './components/decisionTree';
+import NodeInformation from './components/nodeInformation/nodeInformation';
+import Prediction from './components/nodeInformation/prediction';
 import ReactDOM from 'react-dom';
+import Split from './components/nodeInformation/split';
+import Statistics from './components/nodeInformation/ptatistics';
+
+export {
+  DecisionRules,
+  DecisionTree,
+  NodeInformation,
+  Prediction,
+  Statistics,
+  Split
+};
 
 export default DecisionTree;
